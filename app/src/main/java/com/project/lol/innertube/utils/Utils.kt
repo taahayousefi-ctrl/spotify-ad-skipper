25d651e5e9f046650336dc0ea71bbb000a675ad1
taahayousefi-ctrl <taahayousefi@gmail.com>
2026-09-17 00:39:48 +0330

    Upload app/src/main/java/com/project/lol/innertube/utils/Utils.kt

---
+++ app/src/main/java/com/project/lol/innertube/utils/Utils.kt
@@ -0,0 +1,38 @@
+package com.project.lol.innertube.utils
+
+import java.security.MessageDigest
+
+
+
+fun ByteArray.toHex(): String = joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
+
+fun sha1(str: String): String = MessageDigest.getInstance("SHA-1").digest(str.toByteArray()).toHex()
+
+fun parseCookieString(cookie: String): Map<String, String> =
+    cookie.split("; ")
+        .filter { it.isNotEmpty() }
+        .mapNotNull { part ->
+            val splitIndex = part.indexOf('=')
+            if (splitIndex == -1) null
+            else part.substring(0, splitIndex) to part.substring(splitIndex + 1)
+        }
+        .toMap()
+
+fun String.parseTime(): Int? {
+    try {
+        val parts = split(":").map { it.toInt() }
+        if (parts.size == 2) {
+            return parts[0] * 60 + parts[1]
+        }
+        if (parts.size == 3) {
+            return parts[0] * 3600 + parts[1] * 60 + parts[2]
+        }
+    } catch (e: Exception) {
+        return null
+    }
+    return null
+}
+
+fun isPrivateId(browseId: String): Boolean {
+    return browseId.contains("privately")
+}
