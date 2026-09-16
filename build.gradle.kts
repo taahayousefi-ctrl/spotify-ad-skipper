4feb22ae8128797662f03d0fd27dd11914b27d25
taahayousefi-ctrl <taahayousefi@gmail.com>
2026-09-16 20:20:41 +0330

    Upload build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.google.firebase.crashlytics") version "3.0.3" apply false
+}
