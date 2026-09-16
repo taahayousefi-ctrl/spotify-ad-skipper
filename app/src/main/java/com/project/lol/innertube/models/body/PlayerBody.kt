5506ed92f60cbd8972d74e7e5527387defb2cf79
taahayousefi-ctrl <taahayousefi@gmail.com>
2026-09-16 20:29:02 +0330

    Upload app/src/main/java/com/project/lol/innertube/models/body/PlayerBody.kt

---
+++ app/src/main/java/com/project/lol/innertube/models/body/PlayerBody.kt
@@ -0,0 +1,30 @@
+package com.project.lol.innertube.models.body
+
+import com.project.lol.innertube.models.Context
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PlayerBody(
+    val context: Context,
+    val videoId: String,
+    val playlistId: String?,
+    val playbackContext: PlaybackContext? = null,
+    val serviceIntegrityDimensions: ServiceIntegrityDimensions? = null,
+    val contentCheckOk: Boolean = true,
+    val racyCheckOk: Boolean = true,
+) {
+    @Serializable
+    data class PlaybackContext(
+        val contentPlaybackContext: ContentPlaybackContext
+    ) {
+        @Serializable
+        data class ContentPlaybackContext(
+            val signatureTimestamp: Int
+        )
+    }
+
+    @Serializable
+    data class ServiceIntegrityDimensions(
+        val poToken: String
+    )
+}
